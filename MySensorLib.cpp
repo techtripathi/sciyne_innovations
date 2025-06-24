3fbba49c79e3e4851ab53ba367e55d9eb19ef919
vaibhav tripathi <vaibhav.tripathi@havells.com>
2025-06-25 00:33:33 +0530

    added write property for BLE

--- MySensorLib.cpp
+++ MySensorLib.cpp
@@ -3,33 +3,43 @@
 #include <BLEDevice.h>
 #include <BLEUtils.h>
 #include <BLEServer.h>
+
 BLEServer *pServer = nullptr;
 BLECharacteristic *pCharacteristic = nullptr;
 bool deviceConnected = false;
 
-#define SERVICE_UUID "12345678-1234-5678-1234-56789abcdef0"
+#define SERVICE_UUID        "12345678-1234-5678-1234-56789abcdef0"
 #define CHARACTERISTIC_UUID "abcd1234-ab12-cd34-ef56-abcdef123456"
 
-class MyServerCallbacks : public BLEServerCallbacks
-{
-  void onConnect(BLEServer *pServer)
-  {
+// Handle client connection/disconnection
+class MyServerCallbacks : public BLEServerCallbacks {
+  void onConnect(BLEServer *pServer) override {
     deviceConnected = true;
+    Serial.println("Client connected");
   }
 
-  void onDisconnect(BLEServer *pServer)
-  {
+  void onDisconnect(BLEServer *pServer) override {
     deviceConnected = false;
+    Serial.println("Client disconnected");
   }
 };
 
-MySensorLib::MySensorLib(int pin)
-{
+// Handle write from BLE central (like nRF Connect)
+class MyCharacteristicCallbacks : public BLECharacteristicCallbacks {
+  void onWrite(BLECharacteristic *pCharacteristic) override {
+    std::string value = pCharacteristic->getValue();
+    if (!value.empty()) {
+      Serial.print("Received Value from App: ");
+      Serial.println(value.c_str());
+    }
+  }
+};
+
+MySensorLib::MySensorLib(int pin) {
   _pin = pin;
 }
 
-void MySensorLib::sciyne_init()
-{
+void MySensorLib::sciyne_init() {
   Serial.begin(115200);
   delay(1000);
 
@@ -41,9 +51,13 @@ void MySensorLib::sciyne_init()
   BLEService *pService = pServer->createService(SERVICE_UUID);
 
   pCharacteristic = pService->createCharacteristic(
-      CHARACTERISTIC_UUID,
-      BLECharacteristic::PROPERTY_READ |
-          BLECharacteristic::PROPERTY_NOTIFY);
+    CHARACTERISTIC_UUID,
+    BLECharacteristic::PROPERTY_READ |
+    BLECharacteristic::PROPERTY_NOTIFY |
+    BLECharacteristic::PROPERTY_WRITE
+  );
+
+  pCharacteristic->setCallbacks(new MyCharacteristicCallbacks());
 
   pCharacteristic->setValue("0");
   pService->start();
@@ -55,16 +69,20 @@ void MySensorLib::sciyne_init()
   Serial.println("BLE Soil Sensor ready...");
 }
 
-void MySensorLib::sciyne_run()
-{
-  int moisture = 0;
-  moisture = rand();
-   Serial.println(moisture);
-  if (deviceConnected)
-  {
+void MySensorLib::sciyne_run() {
+  static int moisture = 0;
+  Serial.print("Sending moisture: ");
+  Serial.println(moisture);
+
+  if (deviceConnected) {
     String value = String(moisture);
     pCharacteristic->setValue(value.c_str());
     pCharacteristic->notify();
+  }
+
+  moisture++;
+  if (moisture >= 100) {
+    moisture = 0;
   }
 
   delay(2000); // send every 2 seconds
